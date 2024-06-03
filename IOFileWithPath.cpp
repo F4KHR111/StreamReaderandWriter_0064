dc6e50ad87fb7aac3251a64a96ff85a75a13771f
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-06-03 15:03:23 +0700

    Membuat perkondisian membuka dan membaca file

--- IOFileWithPath.cpp
+++ IOFileWithPath.cpp
@@ -29,4 +29,27 @@ int main() {
     }
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
+
+    //Membuka file dalam node membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan s baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
