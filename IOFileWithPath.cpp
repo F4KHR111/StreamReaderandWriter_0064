a99bf9a3e88b0c9f11c297bc95a2bf830385e63e
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-06-03 14:52:32 +0700

    Membuat program untuk memilih file di dalam prosedur utama

--- IOFileWithPath.cpp
+++ IOFileWithPath.cpp
@@ -7,5 +7,13 @@ int main() {
     string baris;
     string NamaFile;
 
-    
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam node menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out)
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
