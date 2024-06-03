2a096cf2d8eaca5b42c7528998c85b737676034b
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-06-03 15:12:35 +0700

    Membuat prosedur utama dalam file exeption standard

---
+++ ExeptionStandard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+//unttuk obyek exeption yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    
+}
