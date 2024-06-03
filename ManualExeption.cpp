3fc777b730d1a49e5b6a386ecbd20934a5e3911c
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-06-03 15:10:02 +0700

    Memuat beberapa fungsi kedalam prosedur utama menggunakan perintah try dan catch

--- ManualExeption.cpp
+++ ManualExeption.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 int main ()
 {
-    
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0,5; //melemparkan sebuar integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a){
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selaininteger maka block ini akan dieksekusi*/
+        cout << "default Pengecualian diexekusi" << endl;
+    }
+
+    return 0;
+
 }
